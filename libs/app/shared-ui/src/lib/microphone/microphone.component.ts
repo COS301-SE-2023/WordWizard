11ce4d2826a94f85e8aca782fa69257e82e1102f
janro13806 <botha.janro@gmail.com>
2023-05-18 20:39:04 +0200

    made progress with microphone. done
    Co-Authored-By: Franco21463702 <116432437+Franco21463702@users.noreply.github.com>

--- libs/app/shared-ui/src/lib/microphone/microphone.component.ts
+++ libs/app/shared-ui/src/lib/microphone/microphone.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'word-wizard-microphone',
@@ -6,5 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./microphone.component.scss'],
 })
 export class MicrophoneComponent {
+  isRecording = false;
+  
+  startRecording(): void {
+    console.log('Start recording');
+    this.isRecording = true;
+  }
 
+  stopRecording(): void {
+    console.log('Stop recording');
+    this.isRecording = false;
+  }
+
+  getButtonClass(): string {
+    return this.isRecording ? 'microphone-button active' : 'microphone-button';
+  }
 }
