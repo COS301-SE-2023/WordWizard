db797fca9f809b7199cdd34a007bc18843b6a411
janro13806 <botha.janro@gmail.com>
2023-05-19 09:54:57 +0200

    changes to reading page,
    added header and reading content

--- libs/app/reading/feature/src/reading.page.ts
+++ libs/app/reading/feature/src/reading.page.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
 })
 export class ReadingPage {
   backgroundImage = 'assets/img/CastleBackground.png';
+  word = 'Magic';
+  imageSrc: string;
+  Level = 'Journeyman';
+
+  constructor() {
+    // Set the image source based on the word
+    this.imageSrc = `assets/img/${this.word}.png`;
+  }
 
 }
