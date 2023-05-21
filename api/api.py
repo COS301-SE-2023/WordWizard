9c2c907d629ff156bb11f22217e03e5fc8e2d135
Adrian-Hawkins <u21669831@tuks.co.za>
2023-05-21 14:04:44 +0200

    Added backend example with vocab

--- api/api.py
+++ api/api.py
@@ -1,42 +1,7 @@
 from fastapi import FastAPI
-from firebase import firebase
-from dotenv import dotenv_values
-import firebase_admin
-
-env_variables = dotenv_values(".env")
-firebase_config = {
-    "apiKey": env_variables['NX_FIREBASE_API_KEY'],
-    "authDomain": env_variables['NX_FIREBASE_AUTH_DOMAIN'],
-    "databaseURL": env_variables['NX_FIREBASE_DATABASE_URL'],
-    "projectId": env_variables['NX_FIREBASE_PROJECT_ID'],
-    "storageBucket": env_variables['NX_FIREBASE_STORAGE_BUCKET'],
-    "messagingSenderId": env_variables['NX_FIREBASE_MESSAGING_SENDER_ID'],
-    "appId": env_variables['NX_FIREBASE_APP_ID'],
-    "measurementId": env_variables['NX_FIREBASE_MEASUREMENT_ID']
-}
-
-# firebase_client = firebase.FirebaseApplication(firebase_config['databaseURL'])
-# print(firebase_config)
-
-cred_obj = firebase_admin.credentials.Certificate(firebase_config)
-default_app = firebase_admin.initialize_app(cred_obj, {
-    'databaseURL':"././././"
-    })
+from .vocab.api.vocab import router as vocab_router
 
 app = FastAPI()
 
-inventory = {
-    1: {
-        "name": "Eggs",
-        "price": 1.99,
-        "brand": "Happy Eggs, Inc.",
-    }
-}
+app.include_router(vocab_router, prefix="/vocab", tags=["vocab"])
 
-@app.get("/")
-def home():
-    return {"Hello": "World"}
-
-@app.get("/about")
-def about():
-    return {"about": "this is about page"}
---
+++ api/vocab/tests/tests_vocab.py
@@ -0,0 +1,12 @@
+import pytest
+import requests
+from fastapi.testclient import TestClient
+
+from api import app
+
+client = TestClient(app)
+
+def test_get_vocab():
+    response = client.get("/vocab")
+    assert response.status_code == 200
+    assert response.json() == {"vocab": "vocab"}
