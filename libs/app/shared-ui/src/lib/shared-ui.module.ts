0dcb4e132d3ec525f8767fabcc91cebb1c6c9ee1
Prometheus18082106 <u21451452@tuks.co.za>
2023-05-22 12:40:56 +0200

    Update shared-ui.module.ts

--- libs/app/shared-ui/src/lib/shared-ui.module.ts
+++ libs/app/shared-ui/src/lib/shared-ui.module.ts
@@ -1,18 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import { MicrophoneModule } from './microphone';
-import { SpeakerModule } from './speaker';
-import { ExitModule } from './exit';
-import { HeaderModule } from './header'
-import { InfoModule } from './info';
-
-@NgModule({
-  imports: [CommonModule, IonicModule, MicrophoneModule, SpeakerModule, HeaderModule, ExitModule, InfoModule],
-  declarations: [],
-  exports: [MicrophoneModule, SpeakerModule, ExitModule, InfoModule, HeaderModule],
-=======
 import { MicrophoneModule } from './microphone/microphone.module';
 import { SpeakerModule } from './speaker/speaker.module';
 import { InfoModule } from './info';
@@ -23,6 +11,7 @@ import { HeaderModule } from './header/header.module';
   imports: [CommonModule, IonicModule, MicrophoneModule, SpeakerModule, HeaderModule, InfoModule],
   exports: [MicrophoneModule, SpeakerModule, InfoModule, HeaderModule],
 
->>>>>>> dev
+  
+
 })
 export class SharedUiModule {}
