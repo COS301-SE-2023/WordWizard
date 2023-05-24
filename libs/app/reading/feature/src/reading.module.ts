99c99f45cadec0e7259973d05fb0bb8c3e8ce3f9
Prometheus18082106 <u21451452@tuks.co.za>
2023-05-24 14:01:27 +0200

    updated imports & added potion to reading template
    Updated the imports & exports for the modules.
    Added the progress potion to the template
    Changed Percentage attribute in reading.page.ts to a string

--- libs/app/reading/feature/src/reading.module.ts
+++ libs/app/reading/feature/src/reading.module.ts
@@ -6,6 +6,7 @@ import { IonicModule } from '@ionic/angular';
 import { ReadingPage } from './reading.page';
 import { ReadingRouting } from './reading.routing';
 import { RouterModule } from '@angular/router';
+import { ReadingModule as ReadingUiModule } from '../../ui/src/reading.module';
 
 import { SharedUiModule } from '@word-wizard/app/shared-ui';
 
@@ -17,6 +18,7 @@ import { SharedUiModule } from '@word-wizard/app/shared-ui';
     ReadingRouting,
     RouterModule,
     SharedUiModule,
+    ReadingUiModule
 
   ],
   declarations: [ReadingPage],
