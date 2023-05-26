7de2bcee1d39bb43c1604ec276d607dcedba7714
janro13806 <botha.janro@gmail.com>
2023-05-26 14:02:26 +0200

    generated signup and login libraries

---
+++ libs/app/auth/feature/src/signup/signup.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SocialAuthService, SocialUser, GoogleLoginProvider } from 'angularx-social-login';
+
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.page.html',
+  styleUrls: ['./signup.page.scss'],
+})
+export class Signup {
+
+
+}
