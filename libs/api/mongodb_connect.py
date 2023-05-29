2be7004da8ad7c863e8613f720713c814891c045
janro13806 <botha.janro@gmail.com>
2023-05-29 19:00:51 +0200

    Updated the connection to MongoDB

--- libs/api/mongodb_connect.py
+++ libs/api/mongodb_connect.py
@@ -1,7 +1,10 @@
 from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
 
 # Connect to MongoDB
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb+srv://DBUser:WWcos301@wordwizardcluster.dx1d9ft.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
 
 # Access the database
 db = client["WordWizardDB"]  # Replace "your_database_name" with your actual database name
