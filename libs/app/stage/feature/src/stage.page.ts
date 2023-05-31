4de4c306a4262cda87aabb5446a1ad8f674f1064
FrancovV <74922600+FrancovV@users.noreply.github.com>
2023-05-31 10:52:55 +0200

    setup stage page

---
+++ libs/app/stage/feature/src/stage.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ww-app-stage',
+  templateUrl: './stage.page.html',
+  styleUrls: ['./stage.page.scss'],
+})
+export class StagePage {
+
+}
