726f9ad65aaaf6a2cf90cc633c53ee8721b474bd
Prometheus18082106 <u21451452@tuks.co.za>
2023-06-04 14:06:37 +0200

    Added DB connection to library api
    Added connection to db code in the library.py file in library/api
    
    Co-Authored-By: janro13806 <80365036+janro13806@users.noreply.github.com>

--- libs/api/library/api/library.py
+++ libs/api/library/api/library.py
@@ -1,6 +1,24 @@
 from fastapi import APIRouter
 from ..util.library_models import PracticeRqst, VocabRqst, Word, WordList
 
+# DB Connection
+from pymongo import MongoClient
+import certifi
+import json
+
+ca = certifi.where()
+
+# Connect to MongoDB
+with open('config.json') as config_file:
+    config_data = json.load(config_file)
+
+connectionString = config_data["Database"]["connectionString"]
+print(connectionString)
+client = MongoClient(connectionString, tlsCAFile=ca)
+
+# Access the database
+db = client["WordWizardDB"] 
+# -----------------------------------------------------------------------
 
 router = APIRouter()
 
@@ -18,6 +36,9 @@ def create_reading(reading: PracticeRqst):
 
 @router.post('/vocab')
 def create_vocab(vocab: VocabRqst):
+
+
+
     wordList = WordList(words=[
         Word(word='vocab', defenition='defenition'), 
         Word(word='word', defenition='defenition')
