e68a24289025bba19f273ee91a685ff04f9caa1b
Prometheus18082106 <u21451452@tuks.co.za>
2023-06-04 14:26:37 +0200

    Changed to harcoded id for db call
    Changed code to test db function call
    
    Co-Authored-By: janro13806 <80365036+janro13806@users.noreply.github.com>

--- libs/api/library/api/library.py
+++ libs/api/library/api/library.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from ..util.library_models import PracticeRqst, VocabRqst, Word, WordList
+# from ..util.library_models import PracticeRqst, VocabRqst, Word, WordList
 
 # DB Connection
 from pymongo import MongoClient
@@ -9,7 +9,7 @@ import json
 ca = certifi.where()
 
 # Connect to MongoDB
-with open('config.json') as config_file:
+with open('../../config.json') as config_file:
     config_data = json.load(config_file)
 
 connectionString = config_data["Database"]["connectionString"]
@@ -22,29 +22,36 @@ db = client["WordWizardDB"]
 
 router = APIRouter()
 
+
 @router.get('/')
 def get_practice():
     return {'reading': 'reading'}
 
-@router.post('/practice')
-def create_reading(reading: PracticeRqst):
-    wordList = WordList(words=[
-        Word(word='practice', defenition='defenition'), 
-        Word(word='word', defenition='defenition')
-    ])
-    return wordList
+# @router.post('/practice')
+# def create_reading(reading: PracticeRqst):
+#     wordList = WordList(words=[
+#         Word(word='practice', defenition='defenition'), 
+#         Word(word='word', defenition='defenition')
+#     ])
+#     return wordList
 
 @router.post('/vocab')
-def create_vocab(vocab: VocabRqst):
-
+def get_vocab():
+    # vocab: VocabRqst
     # Access the collection
     vocab_collection = db["Vocabulary"]  # Replace "user" with your actual collection name
 
     # Get the relevant vocab list
-    result = vocab_collection.find_one({"child_id": vocab.userID})
+    result = vocab_collection.find_one({"child_id": "64784f19bdfa8f92954b9d77"})
+    print(result)
 
-    wordList = WordList(words=[
-        Word(word='vocab', defenition='defenition'), 
-        Word(word='word', defenition='defenition')
-    ])
-    return wordList
+
+    # wordList = WordList(words=[
+    #     Word(word='vocab', defenition='defenition'), 
+    #     Word(word='word', defenition='defenition')
+    # ])
+
+    # wordList = result
+    # return wordList
+
+get_vocab()
