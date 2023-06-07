837cba14cedc92a358f14e1f2ae459cb52108aa9
Adrian-Hawkins <u21669831@tuks.co.za>
2023-06-07 11:34:57 +0200

    Fixed practice endpoint
    Co-Authored-By: janro13806 <80365036+janro13806@users.noreply.github.com>
    Co-Authored-By: Megan Hugo u20538422 <u20538422@tuks.co.za>

--- libs/api/library/api/library.py
+++ libs/api/library/api/library.py
@@ -7,7 +7,8 @@ load_dotenv()
 
 # Access environment variable
 connection_string = os.getenv("MONGODB_CONNECTION_STRING")
-
+client = MongoClient(connection_string)
+db = client["WordWizardDB"]
 # DB Connection
 
 # Connect to MongoDB
@@ -48,34 +49,38 @@ def get_practice():
 
 @router.post('/practice')
 def create_reading(practice: PracticeRqst):
-    client = MongoClient(connection_string)
-    db = client["WordWizardDB"]
-    collection = db["PracticeList"]
-    result = collection.find_one({"child_id": practice.userID})
-    if result is None:
-        print("No practice list found")
-        return None
-    print(result["words"])
-    client.close()
+    Practice_collection = db["Practice"]
+    print(practice.userID)
+    result = Practice_collection.find_one({"child_id": practice.userID})
+    # if result is None:
+    #     print("No practice list found")
+    #     return None
+    for document in result:
+        print("LOOP")
+        print(document)
+
     return {'reading': 'reading'}
 
 
 @router.post('/vocab')
 def get_vocab(vocab: VocabRqst):
-    client = MongoClient(connection_string)
-    db = client["WordWizardDB"]
     # Access the collection
     vocab_collection = db["Vocabulary"]
     # Get the relevant vocab list
     print(vocab.userID)
     result = vocab_collection.find_one({"child_id": vocab.userID})
+    result2 = vocab_collection.find()
     print(result["words"])
 
-    wordList = WordList(words=[
-        Word(word='vocab', defenition='defenition'), 
-        Word(word='word', defenition='defenition')
-    ])
+    for document in result2:
+        print("LOOP")
+        print(document)
 
-    wordList = result
-    client.close()
-    return wordList
+    # wordList = WordList(words=[
+    #     Word(word='vocab', defenition='defenition'), 
+    #     Word(word='word', defenition='defenition')
+    # ])
+
+    # wordList = result
+    # client.close()
+    return result2
