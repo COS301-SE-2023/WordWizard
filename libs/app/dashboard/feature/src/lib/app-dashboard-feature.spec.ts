bbaf3ba3270e74a10c7e80b8b7652aac2b94de55
Prometheus18082106 <u21451452@tuks.co.za>
2023-06-08 15:46:51 +0200

    Generated Dashboard Component

---
+++ libs/app/dashboard/feature/src/lib/app-dashboard-feature.spec.ts
@@ -0,0 +1,7 @@
+import { appDashboardFeature } from './app-dashboard-feature';
+
+describe('appDashboardFeature', () => {
+  it('should work', () => {
+    expect(appDashboardFeature()).toEqual('app-dashboard-feature');
+  });
+});
---
+++ libs/app/dashboard/feature/src/lib/app-dashboard-feature.ts
@@ -0,0 +1,3 @@
+export function appDashboardFeature(): string {
+  return 'app-dashboard-feature';
+}
