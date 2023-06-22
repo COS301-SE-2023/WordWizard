0d220985b852bc82e18193d2068fc58c1a3c4105
Adrian-Hawkins <u21669831@tuks.co.za>
2023-06-22 13:46:37 +0200

    Fixed reading stuff
    Libraries still dont build due to ngmodule issue. I'll fix later sry
    
    Co-Authored-By: Megan Hugo u20538422 <u20538422@tuks.co.za>

--- libs/app/library/ui/src/scroll/scroll.module.ts
+++ libs/app/library/ui/src/scroll/scroll.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ScrollComponent } from './scroll.component';
-import { SharedUiModule } from '@word-wizard/app/shared-ui'
+// import { SharedUiModule } from '@word-wizard/app/shared-ui'
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, SharedUiModule],
+  imports: [CommonModule, FormsModule, IonicModule],
   declarations: [ScrollComponent],
   exports: [ScrollComponent],
 })
