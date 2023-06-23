6e38b777d58f80a9ed0325b45580f5b0a6446d3b
Adrian-Hawkins <u21669831@tuks.co.za>
2023-06-24 00:12:38 +0200

    Why is the reading page giving issues but only in core???

--- libs/app/reading/feature/src/reading.module.ts
+++ libs/app/reading/feature/src/reading.module.ts
@@ -2,17 +2,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
 import { ReadingPage } from './reading.page';
 import { ReadingRouting } from './reading.routing';
-import { RouterModule } from '@angular/router';
-
-import { SharedUiModule } from '@word-wizard/app/shared-ui';
-import { ReadingSharedUiModule } from '@word-wizard/app/reading/shared-ui';
 import { HttpClientModule } from '@angular/common/http';
-import { ReadingService } from '@word-wizard/app/reading/data-access';
+import { RouterModule } from '@angular/router';
 import { NgxsModule } from '@ngxs/store';
-import { ReadingState } from '@word-wizard/app/reading/data-access';
+import { ReadingState, ReadingService } from '@word-wizard/app/reading/data-access';
+import { ReadingSharedUiModule } from '@word-wizard/app/reading/shared-ui';
+
 
 @NgModule({
   imports: [
@@ -21,10 +18,9 @@ import { ReadingState } from '@word-wizard/app/reading/data-access';
     IonicModule,
     ReadingRouting,
     RouterModule,
-    SharedUiModule,
-    ReadingSharedUiModule,
-    HttpClientModule,
     NgxsModule.forFeature([ReadingState]),
+    HttpClientModule,
+    ReadingSharedUiModule
   ],
   declarations: [ReadingPage],
   providers: [ReadingService]
