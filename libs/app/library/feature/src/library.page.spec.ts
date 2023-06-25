208d079d17fd7a13c799dbfa3c0a5c64b671f473
Prometheus18082106 <u21451452@tuks.co.za>
2023-06-25 18:51:48 +0200

    Library page integration testing
    Added some basic integration tests for library page
    
    Co-Authored-By: Franco21463702 <116432437+Franco21463702@users.noreply.github.com>
    Co-Authored-By: Adrian-Hawkins <102908326+Adrian-Hawkins@users.noreply.github.com>

--- libs/app/library/feature/src/library.page.spec.ts
+++ libs/app/library/feature/src/library.page.spec.ts
@@ -1,13 +1,16 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { LibraryPage } from './library.page';
-import { NgxsModule } from '@ngxs/store';
-import { LibraryState, LibraryService } from '@word-wizard/app/library/data-access';
+import { NgxsModule, Store } from '@ngxs/store';
+import { LibraryState, LibraryService, LibraryStateModel, Word } from '@word-wizard/app/library/data-access';
 import { HttpClientModule } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('LibraryPage', () => {
   let component: LibraryPage;
   let fixture: ComponentFixture<LibraryPage>;
+  let store: Store;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -15,18 +18,69 @@ describe('LibraryPage', () => {
       imports: [
         IonicModule.forRoot(),
         NgxsModule.forRoot([LibraryState]),
-        HttpClientModule
+        HttpClientModule,
+        HttpClientTestingModule
       ],
       providers: [LibraryService]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LibraryPage);
     component = fixture.componentInstance;
+
+    store = TestBed.inject(Store);
+    jest.spyOn(store, 'dispatch'); // Spy on the dispatch method of the store
+
+    // Mock the readingState$ observable
+    const mockLibraryState$ = of({
+      Library: {
+        model: {
+          Practice: {
+            words: []
+          },
+          Vocab: {
+            words: []
+          }
+        }
+      }
+    } as LibraryStateModel);
+    jest.spyOn(store, 'select').mockReturnValue(mockLibraryState$); // Use mockReturnValue instead of and.returnValue
+
+
     fixture.detectChanges();
   }));
 
   it('should create the LibraryPage', () => {
     expect(component).toBeTruthy();
   });
-});
 
+  // integration testing:
+
+  it('should set practiceList and vocabList', () => {
+    const vcbTest = {
+      word:"Apple",
+      defeniton:"Defintion"
+    } as Word;
+    
+    const prtcTest = {
+      word:"Bannana",
+      defeniton:"Bannana Defintion"
+    } as Word;
+    
+    const newLibraryState = {
+      Library: {
+        model: {
+          Practice: {
+            words: [prtcTest]
+          },
+          Vocab: {
+            words: [vcbTest]
+          }
+        }
+      }
+    } as LibraryStateModel;
+    const mockLibraryState$ = of(newLibraryState); // Update the mock readingState$
+    jest.spyOn(store, 'select').mockReturnValue(mockLibraryState$); // Spy on the select method and return the updated mock readingState$
+    fixture.detectChanges(); // Trigger change detection
+    expect(component.practice[0]).toBe(prtcTest);
+  });
+})
