03c5eb6d2dca882c0182d183bb787e32bdc32e54
Megan Hugo <bellvlekkie@gmail.com>
2023-06-25 15:45:30 +0200

    added voice-recognition front end test
    Co-Authored-By: Adrian-Hawkins <102908326+Adrian-Hawkins@users.noreply.github.com>

---
+++ libs/app/shared-ui/src/lib/voiceRecognition/voice-recognition.spec.ts
@@ -0,0 +1,25 @@
+import { TestBed } from '@angular/core/testing';
+import { VoiceRecognitionService } from './voice-recognition.service';
+
+describe('VoiceRecognitionService', () => {
+  let service: VoiceRecognitionService;
+  let mockRecognition: any;
+
+  beforeEach(() => {
+    mockRecognition = {
+      start: jest.fn(),
+      stop: jest.fn(),
+      addEventListener: jest.fn(),
+    };
+    (window as any)['webkitSpeechRecognition'] = jest.fn().mockImplementation(() => mockRecognition);
+
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(VoiceRecognitionService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+});
+ 
