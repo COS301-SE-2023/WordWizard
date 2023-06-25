f1d9aae777dee7f2e65e277db2d14b011f036d69
Megan Hugo <bellvlekkie@gmail.com>
2023-06-25 10:14:24 +0200

    reading page front-end tests

---
+++ libs/app/reading/feature/src/reading.page.spec.ts
@@ -0,0 +1,45 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { ReadingPage } from './reading.page';
+import { NgxsModule } from '@ngxs/store';
+import { ReadingState, ReadingService } from '@word-wizard/app/reading/data-access';
+import { ReadingSharedUiModule } from '@word-wizard/app/reading/shared-ui';
+import { SharedUiModule } from '@word-wizard/app/shared-ui';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+
+describe('ReadingPage', () => {
+  let component: ReadingPage;
+  let fixture: ComponentFixture<ReadingPage>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ReadingPage],
+      imports: [
+        IonicModule.forRoot(),
+        NgxsModule.forRoot([ReadingState]),
+        HttpClientModule,
+        SharedUiModule,
+        ReadingSharedUiModule,
+        BrowserModule
+      ],
+      providers: [ReadingService]
+    }).compileComponents();
+
+    // Mock webkitSpeechRecognition
+    const mockRecognition = {
+      start: jest.fn(),
+      stop: jest.fn(),
+      onresult: null,
+    };
+    (window as any).webkitSpeechRecognition = jest.fn().mockImplementation(() => mockRecognition);
+
+    fixture = TestBed.createComponent(ReadingPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create the ReadingPage', () => {
+    expect(component).toBeTruthy();
+  });
+});
