f0bb505dbaa74110db9609bd9334d1362255c9ab
Adrian-Hawkins <u21669831@tuks.co.za>
2023-06-30 15:59:32 +0200

    Added speech recognition api

--- libs/app/shared-ui/src/lib/microphone/microphone.module.ts
+++ libs/app/shared-ui/src/lib/microphone/microphone.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { VoiceRecognitionService } from '../voiceRecognition/voice-recognition.service';
 import { IonicModule } from '@ionic/angular';
 
 import { MicrophoneComponent } from './microphone.component';
@@ -9,6 +9,7 @@ import { MicrophoneComponent } from './microphone.component';
 @NgModule({
   imports: [ CommonModule, FormsModule, IonicModule,],
   declarations: [MicrophoneComponent ],
-  exports: [MicrophoneComponent ]
+  exports: [MicrophoneComponent ],
+  providers: [VoiceRecognitionService]
 })
 export class MicrophoneModule {}
