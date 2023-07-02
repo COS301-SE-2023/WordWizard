b0ec7f4b6a7a424c5c4e18d82fe695b0b584b9a7
Adrian-Hawkins <u21669831@tuks.co.za>
2023-07-02 13:46:32 +0200

    added add-child page form

--- libs/app/add-child/feature/src/add-child.module.ts
+++ libs/app/add-child/feature/src/add-child.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 import { AddChildRouting } from './add-child.routing';
 import { AddChildPage } from './add-child.page';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    AddChildRouting
+    AddChildRouting,
+    IonicModule,
+    ReactiveFormsModule,
   ],
   declarations: [AddChildPage],
 })
--- libs/app/add-child/feature/src/add-child.page.ts
+++ libs/app/add-child/feature/src/add-child.page.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'word-wizard-add-child',
   templateUrl: './add-child.page.html',
   styleUrls: ['./add-child.page.scss'],
 })
-export class AddChildPage {}
+export class AddChildPage {
+  form: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+    age: ['', Validators.required],
+  });
+  visible = false;
+
+  // Grab from state later
+  pictures = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  constructor(private readonly fb: FormBuilder) {}
+
+  controlModal() {
+    this.visible = !this.visible;
+  }
+}
