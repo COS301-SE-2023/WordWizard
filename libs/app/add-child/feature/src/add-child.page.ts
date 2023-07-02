a3b1189a2c7370b342b79e9834e312b6e1c9afc3
Adrian-Hawkins <u21669831@tuks.co.za>
2023-07-02 20:20:00 +0200

    Logic added for form
    Change color of text input

--- libs/app/add-child/feature/src/add-child.page.ts
+++ libs/app/add-child/feature/src/add-child.page.ts
@@ -2,23 +2,31 @@ import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'word-wizard-add-child',
+  selector: 'ww-add-child',
   templateUrl: './add-child.page.html',
   styleUrls: ['./add-child.page.scss'],
 })
 export class AddChildPage {
   form: FormGroup = this.fb.group({
     name: ['', Validators.required],
-    age: ['', Validators.required],
-    pictures: this.fb.array([], [Validators.required]),
+    age: ['', Validators.required]
   });
+  // Set visible to true to debug modal
   visible = false;
+  selectedImage!: string;
 
+  // Remove, variable used for development
+  devImage = 'https://img.freepik.com/free-vector/cute-shiba-inu-dog-wearing-dragon-costume-cartoon-vector-icon-illustration-animal-holiday-isolated_138676-7105.jpg?size=626&ext=jpg&ga=GA1.2.772846284.1688291417&semt=ais';
+  devImage2 = 'https://img.freepik.com/free-vector/cute-young-dragon-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated-premium-vector-flat-cartoon-style_138676-3544.jpg?size=626&ext=jpg&ga=GA1.2.772846284.1688291417&semt=ais';
   // Grab from state later
-  pictures = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  pictures = [this.devImage, this.devImage2,this.devImage, this.devImage2,this.devImage, this.devImage2,this.devImage, this.devImage2,this.devImage ];
   constructor(private readonly fb: FormBuilder) {}
 
   controlModal() {
     this.visible = !this.visible;
+  }
+
+  chooseImage(image: string) {
+    this.selectedImage = image;
   }
 }
