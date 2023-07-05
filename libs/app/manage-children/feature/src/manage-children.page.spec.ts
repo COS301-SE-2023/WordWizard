76a1ba357a5f130862daf4b7d1c9cb470e58c1e9
Megan Hugo <bellvlekkie@gmail.com>
2023-07-05 21:00:02 +0200

    Fixed test

--- libs/app/manage-children/feature/src/manage-children.page.spec.ts
+++ libs/app/manage-children/feature/src/manage-children.page.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ManageChildrenPage } from './manage-children.page';
 import { SharedUiModule } from '@word-wizard/app/shared-ui';
+import { IonicModule } from '@ionic/angular';
 
 describe('ManageChildrenPage', () => {
-  let component: ManageChildrenPage;
+  let component: ManageChildrenPage;  
   let fixture: ComponentFixture<ManageChildrenPage>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ManageChildrenPage, SharedUiModule],
+      imports: [ SharedUiModule, IonicModule.forRoot()],
+      declarations: [ManageChildrenPage],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ManageChildrenPage);
