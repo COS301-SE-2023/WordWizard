51f5f023c32c8f5786b833aefd4e14287f7ff45a
Prometheus18082106 <91665358+Prometheus18082106@users.noreply.github.com>
2023-07-06 18:49:49 +0200

    Updated Splash layout & styling
    Removed white card
    Added lighter backgound darkener

--- libs/app/splash/feature/src/splash.page.ts
+++ libs/app/splash/feature/src/splash.page.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'word-wizard-splash',
   templateUrl: './splash.page.html',
   styleUrls: ['./splash.page.scss'],
 })
-export class SplashPage {}
+export class SplashPage {
+  constructor(private route: Router) {}
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      // this.route.navigate(['/welcome'])
+    }, 2000);
+  }
+}
