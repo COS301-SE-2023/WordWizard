68f0517773b2ffce8fa970a12d45d5a5d28de752
Adrian-Hawkins <u21669831@tuks.co.za>
2023-07-06 22:41:14 +0200

    Preped for dev merge

--- libs/app/child/data-access/src/child.service.spec.ts
+++ libs/app/child/data-access/src/child.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { ChildService } from './child.service';
 
 describe('ChildService', () => {
   let service: ChildService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(ChildService);
   });
 
