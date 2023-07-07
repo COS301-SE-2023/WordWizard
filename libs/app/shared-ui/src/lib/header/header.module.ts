a2e8c8ca869d6bc2609e1b8b5fa230a39036c328
Adrian-Hawkins <u21669831@tuks.co.za>
2023-07-07 15:20:35 +0200

    Preped for merge into dev

--- libs/app/shared-ui/src/lib/header/header.module.ts
+++ libs/app/shared-ui/src/lib/header/header.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { HeaderComponent } from './header.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [ CommonModule, FormsModule, IonicModule, RouterModule],
+  imports: [ CommonModule, IonicModule, RouterModule],
   declarations: [HeaderComponent],
   exports: [HeaderComponent]
 })
