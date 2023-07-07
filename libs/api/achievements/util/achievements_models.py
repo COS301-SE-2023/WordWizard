f4bb5204292b06dc9e807740ee8bf5b328347f6a
janro <botha.janro@gmail.com>
2023-07-07 19:22:05 +0200

    added code for the achievements_models.py

---
+++ libs/api/achievements/util/achievements_models.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class AwardsRqst(BaseModel):
+    userID: str
+
+
+class Award:
+    def __init__(self, goal: int, progress: int, description: str, completed: bool, img: str):
+        self.goal = goal
+        self.progress = progress
+        self.description = description
+        self.completed = completed
+        self.img = img
+
+
+class AwardSection:
+    def __init__(self, name: str):
+        self.name = name
+        self.awards = []  # List to store the awards
+    
+    def add_award(self, award: Award):
+        self.awards.append(award)
+
+
+class AwardsList:
+    def __init__(self):
+        self.awardSections = []
+
+    def add_section(self, section: AwardSection):
+        self.awardSections.append(section)
