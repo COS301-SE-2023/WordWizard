ecd1366fa53b9ce5cbb5f8bed2a00ba79a222e0c
janro <botha.janro@gmail.com>
2023-07-08 11:12:50 +0200

    updated Achievements.py and api.py
    wrote get_Awards() function and added route

--- libs/api/achievements/api/achievements.py
+++ libs/api/achievements/api/achievements.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from ..util.library_models import PracticeRqst, VocabRqst, Word, WordList, UpdateRqst
+from ..util.achievements_models import AwardsRqst, AwardSection, Award, AwardsList
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
@@ -11,3 +11,34 @@ router = APIRouter()
 connection_string = os.getenv("MONGODB_CONNECTION_STRING")
 client = MongoClient(connection_string)
 db = client["WordWizardDB"]
+
+
+@router.post('/practice')
+def get_Awards(rqst: AwardsRqst):
+    progress_collection = db["Progress"]
+    result = progress_collection.find_one({"child_id": rqst.userID})
+    
+    if result is None:
+        return None
+    
+    awards_list = AwardsList() # Create an instance of AwardsList
+    
+    # Retrieve the awards data from the result and create Award objects
+    # Add the Award objects to the respective AwardSections in the AwardsList
+    for section_name, section_data in result["awards"].items():
+        award_section = AwardSection(name=section_name)  # Create an instance of AwardSection
+        
+        for award_name, award_data in section_data.items():
+            goal = award_data["goal"]
+            progress = award_data["progress"]
+            description = award_data["description"]
+            completed = award_data["completed"]
+            img = award_data["img"]
+            
+            award = Award(goal=goal, progress=progress, description=description, completed=completed, img=img)
+            award_section.add_award(award)
+        
+        awards_list.add_section(award_section)
+    
+    return awards_list
+
