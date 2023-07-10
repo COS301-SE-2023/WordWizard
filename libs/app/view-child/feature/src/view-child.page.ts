4f59115fa5551e49759a96fef926c8d4022b6cd2
Prometheus18082106 <91665358+Prometheus18082106@users.noreply.github.com>
2023-07-10 21:11:56 +0200

    View-Child integration with childstate
    Added code to get child data from childstate

--- libs/app/view-child/feature/src/view-child.page.ts
+++ libs/app/view-child/feature/src/view-child.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {
+  SetChild,
+  ChildState,
+  Child
+} from '@word-wizard/app/child/data-access';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'word-wizard-view-child',
@@ -6,6 +13,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-child.page.scss'],
 })
 export class ViewChildPage {
+  @Select(ChildState.currentChild) currentChild$!: Observable<Child>;
+
+  child: any = {
+    username: 'Martie',
+    pfp : 'assets/img/item/cauldron-cropped.png',
+    stage: 0,
+    title: 'Journeyman'
+  };
+  
+  constructor(private store: Store) {
+    this.currentChild$.subscribe((data) => {
+      if (data.profile_photo != '')
+      {
+        this.child.pfp = data.profile_photo;
+      }
+
+      if (data.username != '')
+      {
+        this.child.username = data.username;
+      }
+
+      // Get the stage from the db
+      // Set the child object's stage
+    })
+  }
   stage: number = 1;
   pfp : string = 'assets/img/item/cauldron-cropped.png';
   name: string = "Martie";
