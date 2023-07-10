8b900aaac6c6bc9b67db44624129846fc26f2bb0
Prometheus18082106 <91665358+Prometheus18082106@users.noreply.github.com>
2023-07-10 16:20:42 +0200

    Dashboard - connection to Childstate
    Added imports and functions to get currentchild data from child state
    Updated template to use child object

--- libs/app/dashboard/feature/src/dashboard.page.ts
+++ libs/app/dashboard/feature/src/dashboard.page.ts
@@ -5,6 +5,11 @@ import {
   LibraryState,
   WordList,
 } from '@word-wizard/app/library/data-access';
+import {
+  SetChild,
+  ChildState,
+  Child
+} from '@word-wizard/app/child/data-access';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 @Component({
@@ -13,6 +18,31 @@ import { Observable } from 'rxjs';
   styleUrls: ['./dashboard.page.scss']
 })
 export class DashboardPage {
+  @Select(ChildState.currentChild) currentChild$!: Observable<Child>;
+
+  child: any = {
+    username: '',
+    pfp : 'assets/img/item/cauldron-cropped.png',
+    stage: 0,
+    title: 'Journeyman'
+  };
+
+  constructor(private store: Store) {
+    this.currentChild$.subscribe((data) => {
+      if (data.profile_photo != '')
+      {
+        this.child.pfp = data.profile_photo;
+      }
+
+      if (data.username != '')
+      {
+        this.child.username = data.username;
+      }
+
+      // Get the stage from the db
+      // Set the child object's stage
+    })
+  }
   stage: number = 1;
   pfp : string = 'assets/img/item/cauldron-cropped.png';
 }
