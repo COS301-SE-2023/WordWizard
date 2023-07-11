6afb8fcae6a2040c031980a5da4e585fff1b7089
Prometheus18082106 <91665358+Prometheus18082106@users.noreply.github.com>
2023-07-11 12:22:14 +0200

    Modal component
    Added boilerplate and basic template

---
+++ libs/app/shared-ui/src/lib/modal/modal.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+@Component({
+  selector: 'ww-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss'],
+})
+export class ModalComponent  implements OnInit {
+
+  @Input() modalType: string = '';
+  @Input() title: string = '';
+  @Input() items: string[] = [];
+
+  constructor(private modalController: ModalController) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  dismiss() {
+    this.modalController.dismiss();
+  }
+
+  async openModal(modalType: string) {
+    const modal = await this.modalController.create({
+      component: ModalComponent,
+      componentProps: {
+        modalType,
+        title: 'Custom Modal',
+        items: ['Item 1', 'Item 2', 'Item 3'],
+      },
+    });
+  
+    await modal.present();
+  }
+
+}
