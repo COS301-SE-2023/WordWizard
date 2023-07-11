604f7ea29b6cd609ecaa318013c2dcd80f28708b
Megan Hugo <bellvlekkie@gmail.com>
2023-07-11 22:57:51 +0200

    Updated endpoint to work with ObjectId
    Co-Authored-By: Adrian-Hawkins <102908326+Adrian-Hawkins@users.noreply.github.com>

--- libs/api/achievements/api/achievements.py
+++ libs/api/achievements/api/achievements.py
@@ -6,6 +6,7 @@ from pymongo import MongoClient
 from dataclasses import dataclass
 import mongomock
 load_dotenv()
+from bson import ObjectId
 
 router = APIRouter()
 connection_string = os.getenv("MONGODB_CONNECTION_STRING")
@@ -13,32 +14,23 @@ client = MongoClient(connection_string)
 db = client["WordWizardDB"]
 
 
-@router.post('/achievements')
+
+@router.post('/')
 def get_Awards(rqst: AwardsRqst):
     progress_collection = db["Progress"]
-    result = progress_collection.find_one({"child_id": rqst.userID})
-    
+    result = progress_collection.find_one({"child_id": ObjectId(rqst.child_id)})
     if result is None:
-        return None
-    
-    awards_list = AwardsList() # Create an instance of AwardsList
-    
-    # Retrieve the awards data from the result and create Award objects
-    # Add the Award objects to the respective AwardSections in the AwardsList
+        return { "Status": "Error" }
+    awards_list = AwardsList() 
     for section_name, section_data in result["awards"].items():
-        award_section = AwardSection(name=section_name)  # Create an instance of AwardSection
-        
+        award_section = AwardSection(name=section_name)
         for award_name, award_data in section_data.items():
             goal = award_data["goal"]
             progress = award_data["progress"]
             description = award_data["description"]
             completed = award_data["completed"]
             img = award_data["img"]
-            
             award = Award(goal=goal, progress=progress, description=description, completed=completed, img=img)
             award_section.add_award(award)
-        
         awards_list.add_section(award_section)
-    
     return awards_list
-
