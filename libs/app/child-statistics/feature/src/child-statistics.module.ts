5c41ff2f72582f2707f5c1029df265615e2b3c2b
Adrian-Hawkins <u21669831@tuks.co.za>
2023-07-12 03:20:45 +0200

    Added mock chart, styling for child statistics

--- libs/app/child-statistics/feature/src/child-statistics.module.ts
+++ libs/app/child-statistics/feature/src/child-statistics.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChildStatisticsRouting } from './child-statistics.routing';
 import { ChildStatisticsPage } from './child-statistics.page';
+import { IonicModule } from '@ionic/angular';
+import { SharedUiModule } from '@word-wizard/app/shared-ui';
 
 @NgModule({
   imports: [
     CommonModule,
     ChildStatisticsRouting,
+    IonicModule,
+    SharedUiModule,
   ],
   declarations: [ChildStatisticsPage],
 })
--- libs/app/child-statistics/feature/src/child-statistics.page.ts
+++ libs/app/child-statistics/feature/src/child-statistics.page.ts
@@ -1,8 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef  } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'word-wizard-child-statistics',
   templateUrl: './child-statistics.page.html',
   styleUrls: ['./child-statistics.page.scss'],
 })
-export class ChildStatisticsPage {}
+export class ChildStatisticsPage implements AfterViewInit{
+
+  ngAfterViewInit(): void {
+    // this.renderChart();
+    this.rednerPieChart();
+  }
+  renderChart() {
+    const labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(201, 203, 207, 0.2)'
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)',
+          'rgb(201, 203, 207)'
+        ],
+        borderWidth: 1
+      }]
+    };
+    const ctx = document.getElementById('bar-chart') as HTMLCanvasElement;
+    new Chart(ctx, {
+      type: 'bar',
+      data: data,
+      options: {
+        scales: {
+          y: {
+            type: 'linear',
+            beginAtZero: true
+          }
+        }
+      },
+    });
+  }
+
+  rednerPieChart() {
+    const data = {
+      labels: [
+        'Red',
+        'Blue',
+        'Yellow'
+      ],
+      datasets: [{
+        label: 'My First Dataset',
+        data: [300, 50, 100],
+        fill: false,
+        borderColor: 'rgba(75, 192, 192, 0)',
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+      }]
+    };
+    const ctx = document.getElementById('pie-chart') as HTMLCanvasElement;
+    new Chart(ctx, {
+      type: 'pie',
+      data: data,
+    });
+  }
+}
