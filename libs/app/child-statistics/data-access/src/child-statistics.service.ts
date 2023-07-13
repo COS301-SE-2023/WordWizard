b45107a8b835170d767a59e5056a7023f8b9d5fc
Megan Hugo <bellvlekkie@gmail.com>
2023-07-13 12:05:18 +0200

    stats service added + getStats request

--- libs/app/child-statistics/data-access/src/child-statistics.service.ts
+++ libs/app/child-statistics/data-access/src/child-statistics.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { StatisticsReq } from './requests/child-statistics.requests';
+import { statistics } from './responses/child-statistics.responses';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +9,15 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class ChildStatisticsService {
 
   constructor(private readonly http: HttpClient) { }
+
+  getStats(childId: string) {
+    const request: StatisticsReq = {
+      child_id : childId
+    }
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    return this.http.post<statistics>(`${process.env['WW_API_ENDPOINT']}/add-child`, request, { headers });
+  }
 }
