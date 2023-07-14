15c88f67d6bc4497b292e90e5cfbeb3ac4b10693
Adrian-Hawkins <u21669831@tuks.co.za>
2023-07-14 22:40:53 +0200

    Added more reading unit tests

--- libs/app/reading/feature/src/reading.page.spec.ts
+++ libs/app/reading/feature/src/reading.page.spec.ts
@@ -4,6 +4,7 @@ import { ReadingPage } from './reading.page';
 import { NgxsModule } from '@ngxs/store';
 import { ReadingModule } from './reading.module';
 import { ActivatedRoute } from '@angular/router';
+import { Word } from '@word-wizard/app/reading/data-access';
 
 describe('ReadingPage', () => {
   let component: ReadingPage;
@@ -39,5 +40,20 @@ describe('ReadingPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return "green" when word.correct is true', () => {
+    const word: Word = { word: 'test', imageURL: '', correct: true };
+    expect(component.getWordColor(word)).toEqual('green');
+  });
+
+  it('should return "red" when word.correct is false', () => {
+    const word: Word = { word: 'test', imageURL: '', correct: false };
+    expect(component.getWordColor(word)).toEqual('red');
+  });
+
+  it('should return "white" when word is null or undefined', () => {
+    expect(component.getWordColor(null)).toEqual('white');
+    expect(component.getWordColor(undefined)).toEqual('white');
   });
 });
