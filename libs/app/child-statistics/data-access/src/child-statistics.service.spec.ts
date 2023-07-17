7a3f27799f7625159411426c80ce66c79d3940a1
Megan Hugo <bellvlekkie@gmail.com>
2023-07-17 13:05:50 +0200

    updated tests

--- libs/app/child-statistics/data-access/src/child-statistics.service.spec.ts
+++ libs/app/child-statistics/data-access/src/child-statistics.service.spec.ts
@@ -1,13 +1,25 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ChildStatisticsService } from './child-statistics.service';
+import { StatisticsReq } from './requests/child-statistics.requests';
+import { Statistics } from './responses/child-statistics.responses';
 
 describe('ChildStatisticsService', () => {
   let service: ChildStatisticsService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ChildStatisticsService]
+    });
+
     service = TestBed.inject(ChildStatisticsService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
