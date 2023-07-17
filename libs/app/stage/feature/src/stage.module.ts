b116fdcb8a2fc4feb137c67c987fbba26b737a8e
FrancovV <74922600+FrancovV@users.noreply.github.com>
2023-07-17 14:17:11 +0200

    added header
    Co-Authored-By: janro13806 <80365036+janro13806@users.noreply.github.com>

--- libs/app/stage/feature/src/stage.module.ts
+++ libs/app/stage/feature/src/stage.module.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-import { SharedUiModule } from '@word-wizard/app/stage/shared-ui';
+import { LocalSharedUi } from '@word-wizard/app/stage/shared-ui';
 import { StagePage } from './stage.page';
 import { RouterModule } from '@angular/router';
 import { NgxsModule } from '@ngxs/store';
 import { StageService, StageState } from '@word-wizard/app/stage/data-access';
 import { StageRouting } from './stage.routing';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedUiModule } from '@word-wizard/app/shared-ui';
 
 @NgModule({
-  imports: [ CommonModule, FormsModule, IonicModule, StageRouting, SharedUiModule, RouterModule, HttpClientModule, NgxsModule.forFeature([StageState])],
+  imports: [ CommonModule, FormsModule, IonicModule, StageRouting, SharedUiModule, LocalSharedUi, RouterModule, HttpClientModule, NgxsModule.forFeature([StageState])],
   declarations: [StagePage],
   providers: [StageService]
 })
