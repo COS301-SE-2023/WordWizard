d15a9aa6d4cb7ab6e91f6765a1037dec84c50ebd
FrancovV <74922600+FrancovV@users.noreply.github.com>
2023-07-20 00:00:25 +0200

    fixed backend db calls and frontend request interfaces

--- libs/api/stage/api/stage.py
+++ libs/api/stage/api/stage.py
@@ -3,6 +3,9 @@ from ..util.stage_models import LevelRequest
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
+from bson import ObjectId
+from pydantic import BaseModel
+
 load_dotenv()
 
 router = APIRouter()
@@ -10,13 +13,24 @@ connection_string = os.getenv("MONGODB_CONNECTION_STRING")
 client = MongoClient(connection_string)
 db = client["WordWizardDB"]
 
+class LevelRequest(BaseModel):
+    progress_id: str
+
 @router.post("/get-levels")
 def create_reading(rqst: LevelRequest):
     progress_collection = db["Progress"]
-    result = progress_collection.find_one({"_id": rqst.progress_id})
-    if result:
-        return result.get("progress_history")
-    return {"message": "Progress not found"}
+
+    try:
+        # Convert the provided string progress_id to ObjectId
+        progress_id = ObjectId(rqst.progress_id)
+        result = progress_collection.find_one({"_id": progress_id})
+
+        if result:
+            return result.get("progress_history")
+        return {"message": "Progress not found"}
+
+    except Exception as e:
+        return {"message": "Invalid ObjectId format"}
 
 @router.get("/test")
 def test():
