84b89f16d9c463c85cc06d352b2f9538534cf2cd
Prometheus18082106 <u21451452@tuks.co.za>
2023-07-23 16:29:44 +0200

    Splash animations
    Added animations for image and text
    Added timeout to route to welcome

--- libs/app/splash/feature/src/splash.page.ts
+++ libs/app/splash/feature/src/splash.page.ts
@@ -11,7 +11,7 @@ export class SplashPage {
 
   ngAfterViewInit() {
     setTimeout(() => {
-      // this.route.navigate(['/welcome'])
+      this.route.navigate(['/welcome'])
     }, 2000);
   }
 }
