490dd80d951afba2caca3a68d470ee26922eefe2
FrancovV <74922600+FrancovV@users.noreply.github.com>
2023-07-24 21:04:20 +0200

    added failing tests for library page, needs to be fixed

--- apps/word-wizard-e2e/src/e2e/library.cy.ts
+++ apps/word-wizard-e2e/src/e2e/library.cy.ts
@@ -17,5 +17,34 @@ describe('word-wizard/library', () => {
     cy.visit('/library');
   });
 
+  // it('should display the Vocabulary List when "Vocabulary" segment is selected', () => {
+  //   cy.visit('/library');
+  //   cy.get('ion-segment-button[value="vocab"]').should('have.attr', 'checked');
+  //   cy.get('.vocab-list').should('be.visible');
+  // });
+
+  // it('should display the Practice List when "Practice" segment is selected', () => {
+  //   cy.visit('/library');
+  //   cy.get('ion-segment-button[value="practice"]').click();
+  //   cy.get('.practice-list').should('be.visible');
+  // });
+
+  // it('should display the Practice List when there is practice data', () => {
+  //   cy.intercept('GET', '/api/practice', { words: [{ word: 'Spell1' }, { word: 'Spell2' }] }).as('getPractice');
+  //   cy.visit('/library');
+  //   cy.get('ion-segment-button[value="practice"]').click();
+  //   cy.get('.practice-list').should('be.visible');
+  //   cy.wait('@getPractice');
+  // });
+
+  // it('should display the Vocabulary List when there is vocabulary data', () => {
+  //   cy.intercept('GET', '/api/vocab', { words: [{ word: 'Spell1' }, { word: 'Spell2' }] }).as('getVocab');
+  //   cy.visit('/library');
+  //   cy.get('ion-segment-button[value="vocab"]').should('have.attr', 'checked');
+  //   cy.get('.vocab-list').should('be.visible');
+  //   cy.contains('h3', 'No spells yet :O').should('not.be.visible');
+  //   cy.wait('@getVocab');
+  // });
+
 
 });
