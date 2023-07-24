bd58648bb8e2be08acf5fd7b0708d158f6d42e6d
FrancovV <74922600+FrancovV@users.noreply.github.com>
2023-07-24 15:12:42 +0200

    fixed redirect removing login auth with cypress

--- apps/word-wizard-e2e/src/e2e/stage.cy.ts
+++ apps/word-wizard-e2e/src/e2e/stage.cy.ts
@@ -4,31 +4,26 @@ describe('word-wizard/stage', () => {
   beforeEach(() => {
     cy.viewport('iphone-6')
     cy.visit('/');
-  });
 
-  it('should complete login with Auth0', () => {
     cy.get('ion-button').click();
-
     cy.get('input#username').type(Cypress.env('auth_username'));
     cy.get('input#password').type(Cypress.env('auth_password'), {log: false});
     cy.contains('button', 'Continue').click({force: true});
-
-    /*
-    uncomment for first time login
-    cy.contains('button', 'Accept').click({force: true});
-    */
-
     cy.url().should('equal', 'http://localhost:4200/welcome');
+
   });
 
-
+  it('should load the stage page', () => {
+    cy.visit('/stage');
+  });
 
   it('should display the header title', () => {
     cy.visit('/stage');
-    cy.get('.ww-header-title').should('contain', 'Stages');
+    cy.get('ion-title').should('contain', 'Stages');
   });
 
   // it('should display 20 coins', () => {
+  //   cy.visit('/stage');
   //   cy.get('.ww-lesson-coin')
   //     .find('.coin')
   //     .should('have.length', 20);
