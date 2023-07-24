a403d202ac832c07a6676742cd229e33593f535d
FrancovV <74922600+FrancovV@users.noreply.github.com>
2023-07-24 14:09:55 +0200

    added login e2e tests

--- apps/word-wizard-e2e/src/e2e/app.cy.ts
+++ apps/word-wizard-e2e/src/e2e/app.cy.ts
@@ -1,14 +1,60 @@
 /* eslint-disable */
-// import { getGreeting } from '../support/app.po';
 
-// describe('word-wizard', () => {
-//   beforeEach(() => cy.visit('/'));
+describe('login', () => {
 
-//   it('should display welcome message', () => {
-//     // Custom command example, see `../support/commands.ts` file
-//     cy.login('my-email@something.com', 'myPassword');
+  beforeEach(() => {
 
-//     // Function helper example, see `../support/app.po.ts` file
-//     getGreeting().contains('Welcome word-wizard');
-//   });
-// });
+    cy.viewport('iphone-6');
+
+    cy.visit('/');
+
+  });
+
+  // it('should load the welcome page', () => {
+  //   cy.visit('/welcome');
+  // });
+
+  // it('should successfully log into word-wizard', () => {
+  //   cy.login()
+  //     .then((resp) => {
+  //       return resp.body;
+  //     })
+  //     .then((body) => {
+  //       const {access_token, expires_in, id_token} = body;
+  //       const auth0State = {
+  //         nonce: '',
+  //         state: 'some-random-state'
+  //       };
+  //       const callbackUrl = `/callback#access_token=${access_token}&scope=openid&id_token=${id_token}&expires_in=${expires_in}&token_type=Bearer&state=${auth0State.state}`;
+  //       cy.visit(callbackUrl, {
+
+  //         onBeforeLoad(win) {
+  //           win.document.cookie = 'com.auth0.auth.some-random-state=' + JSON.stringify(auth0State);
+  //         }
+  //       });
+
+
+  //     })
+  // });
+
+  // it('should go to stage page after successful login', () => {
+  //   cy.visit('/welcome');
+  // });
+
+  it('should complete login with Auth0', () => {
+    cy.get('ion-button').click();
+
+    cy.get('input#username').type(Cypress.env('auth_username'));
+    cy.get('input#password').type(Cypress.env('auth_password'), {log: false});
+    cy.contains('button', 'Continue').click({force: true});
+
+    /*
+    uncomment for first time login
+    cy.contains('button', 'Accept').click({force: true});
+    */
+
+    cy.url().should('equal', 'http://localhost:4200/welcome');
+  })
+
+
+});
--- apps/word-wizard-e2e/src/e2e/stage.cy.ts
+++ apps/word-wizard-e2e/src/e2e/stage.cy.ts
@@ -1,29 +1,65 @@
 /* eslint-disable */
 
-describe('word-wizard', () => {
-  beforeEach(() => {
+// describe('word-wizard/stage', () => {
+//   beforeEach(() => {
 
-    cy.viewport('iphone-6')
+//     cy.viewport('iphone-6')
 
-    cy.intercept('GET', 'https://dev-yr27mck7ia3usnqt.us.auth0.com/u/login?state=*', {
-      statusCode: 200,
-      body: {
-        access_token: 'mocked-access-token',
-        token_type: 'Bearer',
-        expires_in: 3600,
-      },
-    }).as('auth0Login');
+//     cy.intercept('GET', 'https://dev-yr27mck7ia3usnqt.us.auth0.com/u/login?state=*', {
+//       statusCode: 200,
+//       body: {
+//         access_token: 'mocked-access-token',
+//         token_type: 'Bearer',
+//         expires_in: 3600,
+//       },
+//     }).as('auth0Login');
 
-    cy.visit('/welcome');
+//     cy.visit('/welcome');
 
 
-  });
+//   });
 
-  it('should access the stage page after successful login', () => {
-    cy.get('ion-button').click();
-    cy.wait('@auth0Login');
+//   it('should access the stage page after successful login', () => {
+//     cy.get('ion-button').click();
+//     cy.wait('@auth0Login');
 
+//     cy.visit('/stage');
+
+//   });
+
+//   it('should display the header title', () => {
+//     cy.visit('/stage');
+//     cy.get('.ww-header-title').should('contain', 'Stages');
+//   });
+
+//   it('should display 20 coins', () => {
+//     cy.get('.ww-lesson-coin')
+//       .find('.coin')
+//       .should('have.length', 20);
+//   });
+
+// });
+
+describe('word-wizard/stage', () => {
+
+  it('should load the stage page', () => {
     cy.visit('/stage');
+    });
 
-  });
+  // it('should access the stage page after successful login', () => {
+  //   //cy.get('ion-button').click();
+  //   cy.visit('/stage');
+
+  // });
+
+  // it('should display the header title', () => {
+  //   cy.get('.ww-header-title').should('contain', 'Stages');
+  // });
+
+  // it('should display 20 coins', () => {
+  //   cy.get('.ww-lesson-coin')
+  //     .find('.coin')
+  //     .should('have.length', 20);
+  // });
+
 });
