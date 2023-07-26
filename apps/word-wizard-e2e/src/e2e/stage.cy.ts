4491d3ab2848f4c7195bf7e963e0b0e1faa281e2
FrancovV <74922600+FrancovV@users.noreply.github.com>
2023-07-26 16:59:56 +0200

    added more tests, some still failing

--- apps/word-wizard-e2e/src/e2e/stage.cy.ts
+++ apps/word-wizard-e2e/src/e2e/stage.cy.ts
@@ -5,11 +5,21 @@ describe('word-wizard/stage', () => {
     cy.viewport('iphone-6')
     cy.visit('/');
 
-    cy.get('ion-button').click();
+    cy.url().then((url) => {
+      if(url.includes('welcome')) {
+        cy.get('ion-button').click();
+      }
+    });
+
     cy.get('input#username').type(Cypress.env('auth_username'));
     cy.get('input#password').type(Cypress.env('auth_password'), {log: false});
     cy.contains('button', 'Continue').click({force: true});
-    cy.url().should('equal', 'http://localhost:4200/welcome');
+
+    cy.get('button').then(($btn) => {
+      if ($btn.text() === 'Accept') {
+        cy.get('button').contains('Accept').click();
+      }
+    });
 
   });
 
