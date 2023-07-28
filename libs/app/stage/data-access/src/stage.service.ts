f1c35c4a96fa839fec888c9a4d2c305c73332093
FrancovV <74922600+FrancovV@users.noreply.github.com>
2023-07-28 10:10:22 +0200

    removed mock data

--- libs/app/stage/data-access/src/stage.service.ts
+++ libs/app/stage/data-access/src/stage.service.ts
@@ -8,30 +8,12 @@ import { levelsRequest } from './requests/stage.requests';
 })
 export class StageService {
 
-  endpoint = "http://127.0.0.1:8000/stage";
-
   constructor(private http:HttpClient) {}
 
   getStage(rqst:levelsRequest): Observable<stage> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json'
     });
-
-    const tempEndpoint = this.endpoint + "/get-levels";
-
-    //return static data for now, placeholder for when backend is ready
-
-    // const tempStage: stage = {
-    //   name: 'Stage 1',
-    //   levels: [3, 3, 2, 1, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    //   background: 'assets/images/stage1.png',
-    //   selectedLevel: 0
-    // }
-
-    // return new Observable<stage>(observer => {
-    //   observer.next(tempStage);
-    //   observer.complete();
-    // });
 
     return this.http.post<stage>(`${process.env['WW_API_ENDPOINT']}/stage/get-levels`, rqst, {headers});
 
