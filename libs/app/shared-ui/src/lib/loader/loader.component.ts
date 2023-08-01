05d26d48b829c989e68295eb06c892cf67b2826c
Prometheus18082106 <u21451452@tuks.co.za>
2023-08-01 19:13:04 +0200

    Loader Component Added
    Created loader component in shared-ui
    Added template and styling

---
+++ libs/app/shared-ui/src/lib/loader/loader.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ww-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.scss'],
+})
+export class LoaderComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
