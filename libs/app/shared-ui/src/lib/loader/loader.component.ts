643386d55feac55e83dffe194288a9dd9cc2e0ad
Prometheus18082106 <u21451452@tuks.co.za>
2023-08-13 18:30:23 +0200

    Loader removed onInit

--- libs/app/shared-ui/src/lib/loader/loader.component.ts
+++ libs/app/shared-ui/src/lib/loader/loader.component.ts
@@ -5,13 +5,11 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.scss'],
 })
-export class LoaderComponent  implements OnInit {
+export class LoaderComponent  {
 
   @Input() visible = false;
   @Input() message = "Loading...";
 
   constructor() { }
-
-  ngOnInit() {}
 
 }
