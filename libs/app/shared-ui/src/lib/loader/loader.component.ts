d88e337d16932cf05a821f34af3ff2dc98173e7e
Prometheus18082106 <u21451452@tuks.co.za>
2023-08-13 18:38:18 +0200

    Linting issues fixed

--- libs/app/shared-ui/src/lib/loader/loader.component.ts
+++ libs/app/shared-ui/src/lib/loader/loader.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'ww-loader',
@@ -9,7 +9,5 @@ export class LoaderComponent  {
 
   @Input() visible = false;
   @Input() message = "Loading...";
-
-  constructor() { }
 
 }
