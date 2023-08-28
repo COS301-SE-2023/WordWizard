5fc95d0761a98b49605cf952125a0d6a0b6c790f
Adrian-Hawkins <u21669831@tuks.co.za>
2023-08-28 22:20:01 +0200

    removed debug
    Co-authored-by: Megan Hugo u20538422 <u20538422@tuks.co.za>

--- libs/api/reading/util/passage.py
+++ libs/api/reading/util/passage.py
@@ -8,7 +8,6 @@ openai.api_key = api_key
 
 def query(query:str):
     q = "Sentence: The wizards subbed their toys.\nFocus Words: wizards, subbed"
-    print(query)
     # q = query_chat(query)
     return santise_string(q)
 
