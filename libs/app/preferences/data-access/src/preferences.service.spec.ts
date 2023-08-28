aff21bee37b82533713fe8553eccb3e61617b728
Adrian-Hawkins <u21669831@tuks.co.za>
2023-08-28 22:31:16 +0200

    fixed tests
    Co-Authored-By: Megan Hugo u20538422 <u20538422@tuks.co.za>

--- libs/app/preferences/data-access/src/preferences.service.spec.ts
+++ libs/app/preferences/data-access/src/preferences.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { PreferencesService } from './preferences.service';
 
 describe('PreferencesService', () => {
   let service: PreferencesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(PreferencesService);
   });
 
--- libs/app/preferences/feature/src/preferences.page.spec.ts
+++ libs/app/preferences/feature/src/preferences.page.spec.ts
@@ -1,9 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PreferencesPage } from './preferences.page';
 import { HeaderModule, ButtonModule } from '@word-wizard/app/shared-ui';
+import { HttpClientModule } from '@angular/common/http';
+import { PreferencesService } from '@word-wizard/app/preferences/data-access';
 import { ActivatedRoute } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
+import { ChildState } from '@word-wizard/app/child/data-access';
+import { NgxsModule } from '@ngxs/store';
 
 describe('PreferencesPage', () => {
   let component: PreferencesPage;
@@ -11,7 +15,7 @@ describe('PreferencesPage', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports:[HeaderModule, ButtonModule, IonicModule, FormsModule ],
+      imports:[HeaderModule, ButtonModule, IonicModule, FormsModule, HttpClientModule, NgxsModule.forRoot([]) ],
       declarations: [PreferencesPage],
       providers: [
         {
@@ -24,6 +28,7 @@ describe('PreferencesPage', () => {
             },
           },
         },
+        PreferencesService,
       ],
     }).compileComponents();
 
