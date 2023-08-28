74f83c7b70ffc79dc0e6bec4abbeccec087ee8e5
Adrian-Hawkins <u21669831@tuks.co.za>
2023-08-28 19:03:53 +0200

    fixed preference tests

--- libs/app/preferences/feature/src/preferences.page.spec.ts
+++ libs/app/preferences/feature/src/preferences.page.spec.ts
@@ -1,5 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PreferencesPage } from './preferences.page';
+import { HeaderModule, ButtonModule } from '@word-wizard/app/shared-ui';
+import { ActivatedRoute } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 describe('PreferencesPage', () => {
   let component: PreferencesPage;
@@ -7,7 +11,20 @@ describe('PreferencesPage', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[HeaderModule, ButtonModule, IonicModule, FormsModule ],
       declarations: [PreferencesPage],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: () => '1',
+              },
+            },
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PreferencesPage);
