8c12f10e500bc0367065c0ec29a72225404cd110
Adrian-Hawkins <u21669831@tuks.co.za>
2023-08-29 14:34:01 +0200

    Added code cov for python tests

--- apps/word-wizard-e2e/src/plugins/index.js
+++ apps/word-wizard-e2e/src/plugins/index.js
@@ -1,6 +1,3 @@
-// cypress/plugins/index.js
-const codeCoverageTask = require('@cypress/code-coverage/task');
-
 module.exports = (on, config) => {
-  on('task', codeCoverageTask);
+  on('task', require('@cypress/code-coverage/task'));
 };
