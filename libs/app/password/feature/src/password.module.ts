3a9d5a3257cd42db7588ad8d07f5cfd78ee81a3e
Megan Hugo <bellvlekkie@gmail.com>
2023-08-30 22:29:19 +0200

    added submit button and password logic
    Co-Authored-By: Adrian-Hawkins <102908326+Adrian-Hawkins@users.noreply.github.com>

--- libs/app/password/feature/src/password.module.ts
+++ libs/app/password/feature/src/password.module.ts
@@ -6,9 +6,10 @@ import { SharedUiModule } from '@word-wizard/app/shared-ui';
 import { IonicModule } from '@ionic/angular';
 import { ReadingSharedUiModule } from '@word-wizard/app/reading/shared-ui';
 import { PasswordRoute } from './password.routing';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, SharedUiModule, IonicModule, ReadingSharedUiModule, PasswordRoute],
+  imports: [CommonModule, RouterModule, SharedUiModule, IonicModule, ReadingSharedUiModule, PasswordRoute, FormsModule,],
   declarations: [PasswordPage],
   exports: [PasswordPage],
 })
--- libs/app/password/feature/src/password.page.ts
+++ libs/app/password/feature/src/password.page.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
+
+
+
+
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'word-wizard-password',
   templateUrl: './password.page.html',
   styleUrls: ['./password.page.scss'],
 })
-export class PasswordPage {}
+
+
+
+export class PasswordPage {
+  password =  '';
+
+  constructor(private router: Router) {}
+
+  isPasswordValid(): boolean {
+    return /^\d{4}$/.test(this.password);
+  }
+
+}
