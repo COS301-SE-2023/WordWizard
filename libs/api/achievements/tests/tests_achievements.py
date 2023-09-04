3e5e9a3bd3d783c0a9df54e73de139caabd129d0
janro <botha.janro@gmail.com>
2023-09-04 15:09:46 +0200

    started test of achievements library

---
+++ libs/api/achievements/tests/tests_achievements.py
@@ -0,0 +1,21 @@
+from fastapi.testclient import TestClient
+from ...test_api import app
+from mongomock import MongoClient
+import mongomock
+from bson.objectid import ObjectId
+
+# Create a TestClient instance for your FastAPI app
+client = TestClient(app)
+
+# Define a test function for get_Awards
+def test_get_Awards():
+    # Create a test request body (you may need to adjust this to match your data structure)
+    rqst_body = {
+        "child_id": "64c786a89a3bb62b5e333daa"  
+    }
+    # Send a POST request to the /achievements endpoint
+    response = client.post("/achievements/", json=rqst_body)
+    assert response.status_code == 200
+    response_data = response.json()
+    assert "awardSections" in response_data
+    assert isinstance(response_data["awardSections"], list)
