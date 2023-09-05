ca0a7cb049be9b3f24b4bb582515d9dad486ef3d
janro13806 <80365036+janro13806@users.noreply.github.com>
2023-09-05 13:36:38 +0200

    Finished Achievements test

--- libs/api/achievements/tests/tests_achievements.py
+++ libs/api/achievements/tests/tests_achievements.py
@@ -1,12 +1,12 @@
 from fastapi.testclient import TestClient
 from ...test_api import app
 
-# Create a TestClient instance for your FastAPI app
+
 client = TestClient(app)
 
-# Define a test function for get_Awards
+
 def test_get_Awards():
-    # Create a test request body (you may need to adjust this to match your data structure)
+    
     rqst_body = {
         "child_id": "64c786a89a3bb62b5e333daa"  
     }
@@ -15,5 +15,30 @@ def test_get_Awards():
     assert response.status_code == 200
     response_data = response.json()
 
-    # Check if the response contains any data
-    assert response_data, "Response data is empty"
+    # Define the expected structure
+    expected_structure = [
+        {
+            'name': str,
+            'awards': [
+                {
+                    'goal': int,
+                    'progress': int,
+                    'description': str,
+                    'completed': bool,
+                    'img': str
+                }
+            ]
+        }
+    ]
+
+    # Check if the response data matches the expected structure
+    assert isinstance(response_data, list), "Response data is not a list"
+    for item in response_data:
+        assert all(key in item for key in expected_structure[0]), "Response item structure is incorrect"
+        for award in item['awards']:
+            assert all(key in award for key in expected_structure[0]['awards'][0]), "Award structure is incorrect"
+
+    for item in response_data:
+        assert isinstance(item['name'], str) and item['name'], "Name is invalid"
+        for award in item['awards']:
+            assert isinstance(award['description'], str) and award['description'], "Award description is invalid"
