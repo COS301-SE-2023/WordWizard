a0d45a6fe4a5f19b134e12a6687a40d5d2d33b7c
janro <botha.janro@gmail.com>
2023-09-06 10:57:42 +0200

    finished with basic add child tests

--- libs/api/add_child/tests/tests_add_child.py
+++ libs/api/add_child/tests/tests_add_child.py
@@ -23,3 +23,20 @@ def test_create_progress():
     response = client.post('/add-child/create-progress')
     assert response.status_code == 200
 
+def test_add_create():
+    rqst_body = AddChildRqst(
+        parent_email=sample_parent_email,
+        parent_name=sample_parent_name,
+        name=sample_child_name,
+        age=sample_child_age,
+        profile_picture=sample_profile_picture
+    )
+    response = client.post('/add-child', json=rqst_body)
+    assert response.status_code == 200
+    assert response.json() is not None
+
+def test_get_photos():
+    response = client.get('/add-child/')
+    assert response.status_code == 200
+    assert 'images' in response.json()
+    assert len(response.json()['images']) > 0
