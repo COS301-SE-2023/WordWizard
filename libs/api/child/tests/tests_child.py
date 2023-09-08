42bc6564abc39144b20c0804b6264cb98854182b
janro <botha.janro@gmail.com>
2023-09-08 12:15:40 +0200

    started basic tests for child

---
+++ libs/api/child/tests/tests_child.py
@@ -0,0 +1,26 @@
+from fastapi.testclient import TestClient
+from . import app  # Import your FastAPI app
+import pytest
+
+# Create a TestClient instance
+client = TestClient(app)
+
+# Define sample data for testing
+sample_parent_email = "parent@example.com"
+sample_parent_name = "Parent Name"
+sample_child_id = "mocked_object_id"  # Replace with a valid child_id
+sample_preferences = ["Preference1", "Preference2"]
+sample_child_name = "Child Name"
+sample_child_age = 5
+sample_profile_picture = "profile.jpg"
+
+
+def test_get_topics():
+    response = client.get("/child/")
+    assert response.status_code == 200
+    assert "topics" in response.json()
+    assert len(response.json()["topics"]) > 0
+
+
+
+
