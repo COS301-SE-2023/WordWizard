7df7c903054c59cbee57d365bedcad90fbd7b17a
janro13806 <80365036+janro13806@users.noreply.github.com>
2023-09-11 22:51:08 +0200

    removed comments

--- libs/api/parent/tests/tests_parent.py
+++ libs/api/parent/tests/tests_parent.py
@@ -1,4 +1,3 @@
-# Import the necessary modules
 from fastapi.testclient import TestClient
 from ..util.parent_models import DeleteParentRqst
 from ...test_api import app
@@ -7,22 +6,14 @@ import json
 
 client = TestClient(app)
 
-# Define sample data for testing
 sample_parent_email = "parent@example.com"
 
 def test_delete_parent_success():
-    # Create a request body
     rqst_body = DeleteParentRqst(parent_email=sample_parent_email)
-
-    # Convert the request body to a dictionary
     rqst_dict = rqst_body.dict()
 
-    # Send a POST request to delete the parent
     response = client.post("/parent/delete", json=rqst_dict)
 
-    # Check if the response status code is 200 (OK)
     assert response.status_code == 200
-
-    # Check if the response contains the expected data
     expected_data = {"status": "success"}
     assert response.json() == expected_data
