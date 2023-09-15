254a979dff3fddcb399b19abd8e989c43871873e
Adrian-Hawkins <u21669831@tuks.co.za>
2023-09-15 13:10:58 +0200

    Auth guard

--- libs/app/core/feature/src/core.routing.ts
+++ libs/app/core/feature/src/core.routing.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AuthGuard, AuthService } from '@auth0/auth0-angular';
+// import { AuthGuard, AuthService } from '@auth0/auth0-angular';
+import { AuthGuard } from './auth.guard';
 import { Router } from '@angular/router';
 
 const routes: Routes = [
@@ -10,7 +11,7 @@ const routes: Routes = [
       import('@word-wizard/app/dashboard/feature').then(
         (m) => m.DashboardModule,
       ),
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: '',
@@ -21,19 +22,19 @@ const routes: Routes = [
     path: 'reading',
     loadChildren: () =>
       import('@word-wizard/app/reading/feature').then((m) => m.ReadingModule),
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'library',
     loadChildren: () =>
       import('@word-wizard/app/library/feature').then((m) => m.LibraryModule),
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'stage',
     loadChildren: () =>
       import('@word-wizard/app/stage/feature').then((m) => m.StageModule),
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'add-child',
@@ -41,7 +42,7 @@ const routes: Routes = [
       import('@word-wizard/app/add-child/feature').then(
         (m) => m.AddChildModule,
       ),
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'manage-children',
@@ -49,7 +50,7 @@ const routes: Routes = [
       import('@word-wizard/app/manage-children/feature').then(
         (m) => m.ManageChildrenModule,
       ),
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'welcome',
@@ -62,13 +63,13 @@ const routes: Routes = [
       import('@word-wizard/app/achievements/feature').then(
         (m) => m.AchievementModule,
       ),
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'settings',
     loadChildren: () =>
       import('@word-wizard/app/child-settings/feature').then((m) => m.ChildSettingsModule),
-      canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'loading',
@@ -86,7 +87,7 @@ const routes: Routes = [
       import('@word-wizard/app/child-statistics/feature').then(
         (m) => m.ChildStatisticsModule,
       ),
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'view-child',
@@ -94,7 +95,7 @@ const routes: Routes = [
       import('@word-wizard/app/view-child/feature').then(
         (m) => m.ViewChildModule,
       ),
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'preferences',
@@ -102,13 +103,12 @@ const routes: Routes = [
       import('@word-wizard/app/preferences/feature').then(
         (m) => m.PreferencesModule,
       ),
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'user-agreement',
     loadChildren: () =>
       import('@word-wizard/app/user-agreement/feature').then((m) => m.UserAgreementModule),
-      // canActivate: [AuthGuard],
   },
   {
     path: 'password',
--- libs/app/login/feature/src/login.page.ts
+++ libs/app/login/feature/src/login.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '@word-wizard/app/auth/data-access';
 
 @Component({
   selector: 'word-wizard-login',
@@ -8,14 +10,21 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginPage {
   form!: FormGroup;
-  constructor(private readonly fb: FormBuilder) {
+  constructor(private readonly fb: FormBuilder, private readonly auth: AuthService, private readonly router: Router) {
     this.form = this.fb.group({
-      email: ['', Validators.required, Validators.email],
-      password: ['', Validators.required, Validators.minLength(8)],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
     });
   }
 
   submit() {
-    console.log("fjsdhf");
+    this.auth.getToken(this.form.value.email, this.form.value.password).subscribe(
+      // eslint-disable-next-line
+      (response: any) => {
+        if(response.access_token) {
+          this.router.navigate(['/manage-children']);
+        }
+      }
+    );
   }
 }
