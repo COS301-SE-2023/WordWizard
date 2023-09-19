36270e9e43b6bdfd9aa2e7046eb28431e8e5b208
Prometheus18082106 <u21451452@tuks.co.za>
2023-09-19 22:58:16 +0200

    Password page testing file

---
+++ libs/app/password/feature/src/password.page.spec.ts
@@ -0,0 +1,30 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PasswordPage } from './password.page';
+import { IonicModule } from '@ionic/angular';
+import { SharedUiModule } from '../../../shared-ui/src';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('PasswordPage', () => {
+  let component: PasswordPage;
+  let fixture: ComponentFixture<PasswordPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        IonicModule.forRoot(),
+        SharedUiModule,
+        RouterTestingModule
+      ],
+      declarations: [PasswordPage],
+
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PasswordPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
