35c349442d84163207af3333a63de9f261f20ff0
janro13806 <80365036+janro13806@users.noreply.github.com>
2023-09-19 23:19:06 +0200

    Update tests_child.py

--- libs/api/child/tests/tests_child.py
+++ libs/api/child/tests/tests_child.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 
 sample_parent_email = "parent@example.com"
 sample_parent_name = "Parent Name"
-sample_child_id = "64fc454dc889d0d030785db0"  
+sample_child_id = "65087dc330bd3697ca68e1a6"  
 sample_preferences = ["Preference1", "Preference2"]
 sample_child_name = "Child Name"
 sample_child_age = 5
@@ -61,27 +61,21 @@ def test_edit_child():
     assert "status" in response.json() and response.json()["status"] == "success"
 
 def test_delete_child():
-    # Define a sample child ID to delete
-    sample_child_id = "64fc454dc889d0d030785db0"  # Replace with a valid child ID
+    sample_child_id = "65087dc330bd3697ca68e1a6"  # Replace with a valid child ID
 
     # Simulate a successful deletion using the mock function
     # You can customize the return value to simulate different scenarios
     mock_delete_child_from_db_result = {'status': 'success'}
-
-    # Mock the delete function to return the result
     app.dependency_overrides[delete] = lambda: mock_delete_child_from_db_result
 
-    # Send a request to delete the child
     response = client.post('/child/delete-child', json={"child_id": sample_child_id})
 
-    # Assert the response status code and content
     assert response.status_code == 200
     assert response.json() == mock_delete_child_from_db_result
 
 
 def mock_delete_child_from_db(child_id):
     # Simulate the deletion process (e.g., marking as deleted)
-    # In this example, we'll return a success message
     return {'status': 'success'}
 
         
