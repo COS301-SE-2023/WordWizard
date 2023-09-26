2933e10a4fffb344999537db9b7e7786b73d65b6
FrancovV <u21463702@tuks.co.za>
2023-09-26 19:32:54 +0200

    added more tests for login page

--- apps/word-wizard-e2e/src/e2e/app.cy.ts
+++ apps/word-wizard-e2e/src/e2e/app.cy.ts
@@ -4,20 +4,26 @@ describe('login', () => {
   beforeEach(() => {
     cy.viewport('iphone-6');
     cy.visit('/');
-  });
-
-
-
-
-  it('should complete login with Auth0', () => {
-    cy.viewport('iphone-6');
-    cy.visit('/');
 
     cy.url().then((url) => {
       if (url.includes('welcome')) {
         cy.get('ion-button').click();
       }
     });
+  });
+
+
+  it('should navigate to Forgot Password', () => {
+    cy.get('#forgot-password').click();
+    cy.url().should('include', 'forgot-password');
+  });
+
+  it('should navigate to SignUp', () => {
+    cy.get('#sign-up').click();
+    cy.url().should('include', 'sign-up');
+  });
+
+  it('should complete login', () => {
 
     cy.get('input#name').type(Cypress.env('auth_username'));
     cy.get('input#age').type(Cypress.env('auth_password'), { log: false });
