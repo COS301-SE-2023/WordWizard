75d5003f85995b3ae23a2e75d06c321066306dff
FrancovV <u21463702@tuks.co.za>
2023-09-27 15:48:20 +0200

    added e2e tests for library

--- apps/word-wizard-e2e/src/e2e/library.cy.ts
+++ apps/word-wizard-e2e/src/e2e/library.cy.ts
@@ -1,61 +1,50 @@
 /* eslint-disable */
 
 describe('word-wizard/library', () => {
-  // beforeEach(() => {
-  //   cy.viewport('iphone-6');
-  //   cy.visit('/');
+  beforeEach(() => {
+    cy.viewport('iphone-6');
+    cy.visit('/');
 
-  //   cy.url().then((url) => {
-  //     if (url.includes('welcome')) {
-  //       cy.get('ion-button').click();
-  //     }
-  //   });
+    cy.url().then((url) => {
+      if (url.includes('welcome')) {
+        cy.get('ion-button').click();
+      }
+    });
 
-  //   cy.get('input#username').type(Cypress.env('auth_username'));
-  //   cy.get('input#password').type(Cypress.env('auth_password'), { log: false });
-  //   cy.contains('button', 'Continue').click({ force: true });
+    cy.get('input#name').type(Cypress.env('auth_username'));
+    cy.get('input#age').type(Cypress.env('auth_password'), { log: false });
+    cy.get('#login-button').click({ force: true });
 
-  //   cy.get('button').then(($btn) => {
-  //     if ($btn.text() === 'Accept') {
-  //       cy.get('button').contains('Accept').click();
-  //     }
-  //   });
-  // });
+    cy.wait(5000);
+    cy.get('button.circle').first().click();
+    cy.get('.continueChild').first().click();
+    cy.get("#otp1").type('1');
+    cy.get("#otp2").type('2');
+    cy.get("#otp3").type('3');
+    cy.get("#otp4").type('4');
 
-  // it('should load the library page', () => {
-  //   cy.visit('/library');
-  // });
+    cy.get('a.spellbook').first().click();
+  });
 
-  // it('should display the Vocabulary List when "Vocabulary" segment is selected', () => {
-  //   cy.visit('/library');
-  //   cy.get('ion-segment-button[value="vocab"]').should('have.attr', 'checked');
-  //   cy.get('.vocab-list').should('be.visible');
-  // });
+  it('should visit the library page', () => {
+    cy.url().should('equal', 'http://localhost:4200/library');
+  });
 
-  // it('should display the Practice List when "Practice" segment is selected', () => {
-  //   cy.visit('/library');
-  //   cy.get('ion-segment-button[value="practice"]').click();
-  //   cy.get('.practice-list').should('be.visible');
-  // });
+  it('should display the header title', () => {
+    cy.get('ion-title').should('contain', 'Library');
+  });
 
-  // it('should display the Practice List when there is practice data', () => {
-  //   cy.intercept('GET', '/api/practice', {
-  //     words: [{ word: 'Spell1' }, { word: 'Spell2' }],
-  //   }).as('getPractice');
-  //   cy.visit('/library');
-  //   cy.get('ion-segment-button[value="practice"]').click();
-  //   cy.get('.practice-list').should('be.visible');
-  //   cy.wait('@getPractice');
-  // });
+  it('should load the scrolls', () => {
+    cy.get('.scroll-closed').should('exist');
+  });
 
-  // it('should display the Vocabulary List when there is vocabulary data', () => {
-  //   cy.intercept('GET', '/api/vocab', {
-  //     words: [{ word: 'Spell1' }, { word: 'Spell2' }],
-  //   }).as('getVocab');
-  //   cy.visit('/library');
-  //   cy.get('ion-segment-button[value="vocab"]').should('have.attr', 'checked');
-  //   cy.get('.vocab-list').should('be.visible');
-  //   cy.contains('h3', 'No spells yet :O').should('not.be.visible');
-  //   cy.wait('@getVocab');
-  // });
+  it('should open a scroll', () => {
+    cy.get('.scroll-closed').first().click();
+    cy.get('.open-scroll-content').should('exist');
+  });
+
+  it('should open practice list and load cauldrons', () => {
+    cy.get('ion-segment-button').last().click();
+    cy.get('ww-cauldron').should('exist');
+  })
 });
