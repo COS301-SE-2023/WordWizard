b11d1f5e3a4d01971807dc57ff0d511ad2723214
FrancovV <u21463702@tuks.co.za>
2023-09-27 12:58:03 +0200

    e2e tests for achievements page

--- apps/word-wizard-e2e/src/e2e/achievements.cy.ts
+++ apps/word-wizard-e2e/src/e2e/achievements.cy.ts
@@ -14,20 +14,18 @@ describe('word-wizard/achievements', () => {
     cy.get('input#name').type(Cypress.env('auth_username'));
     cy.get('input#age').type(Cypress.env('auth_password'), { log: false });
     cy.get('#login-button').click({ force: true });
+
+    cy.wait(3000);
+    cy.get('button.circle').first().click();
+    cy.get('.continueChild').first().click();
+    cy.get("#otp1").type('1');
+    cy.get("#otp2").type('2');
+    cy.get("#otp3").type('3');
+    cy.get("#otp4").type('4');
   });
 
   //select a child first to get to the achievements page
-  it('should select a child, redirect to dashboard, go to achievements and view one', () => {
-    cy.visit('/manage-children');
-    cy.get('.circle').first().click();
-
-    cy.url().then((url) => {
-      if (url.includes('add-child')) {
-        cy.log('no existing children');
-      } else {
-        cy.get('.overlay').should('be.visible');
-        cy.get('.continueChild').click();
-        cy.url().should('equal', 'http://localhost:4200/dashboard');
+it('should select a child, redirect to dashboard, go to achievements and view one', () => {
 
         cy.get('.trophy').first().click();
         cy.url().should('equal', 'http://localhost:4200/achievements');
@@ -41,7 +39,5 @@ describe('word-wizard/achievements', () => {
         );
         cy.get('button').first().click({ force: true });
         cy.get('ww-modal').should('not.be.visible');
-      }
     });
   });
-});
