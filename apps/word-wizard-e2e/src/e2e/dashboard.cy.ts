2e7669018ab2fbfc1f7372bbad2ffef54753d135
FrancovV <u21463702@tuks.co.za>
2023-09-27 17:03:31 +0200

    added manage children e2e tests

--- apps/word-wizard-e2e/src/e2e/dashboard.cy.ts
+++ apps/word-wizard-e2e/src/e2e/dashboard.cy.ts
@@ -14,7 +14,7 @@ describe('word-wizard/dashboard', () => {
     cy.get('input#age').type(Cypress.env('auth_password'), { log: false });
     cy.get('#login-button').click({ force: true });
 
-    cy.wait(3000);
+    cy.wait(4000);
     cy.get('button.circle').first().click();
     cy.get('.continueChild').first().click();
     cy.get("#otp1").type('1');
--- apps/word-wizard-e2e/src/e2e/manage-children.cy.ts
+++ apps/word-wizard-e2e/src/e2e/manage-children.cy.ts
@@ -1,52 +1,34 @@
 /* eslint-disable */
 
 describe('word-wizard/manage-children', () => {
-  // beforeEach(() => {
-  //   cy.viewport('iphone-6');
-  //   cy.visit('/');
+  beforeEach(() => {
+    cy.viewport('iphone-6');
+    cy.visit('/');
 
-  //   cy.url().then((url) => {
-  //     if (url.includes('welcome')) {
-  //       cy.get('ion-button').click();
-  //     }
-  //   });
+    cy.url().then((url) => {
+      if (url.includes('welcome')) {
+        cy.get('ion-button').click();
+      }
+    });
 
-  //   cy.get('input#username').type(Cypress.env('auth_username'));
-  //   cy.get('input#password').type(Cypress.env('auth_password'), { log: false });
-  //   cy.contains('button', 'Continue').click({ force: true });
+    cy.get('input#name').type(Cypress.env('auth_username'));
+    cy.get('input#age').type(Cypress.env('auth_password'), { log: false });
+    cy.get('#login-button').click({ force: true });
 
-  //   cy.get('button').then(($btn) => {
-  //     if ($btn.text() === 'Accept') {
-  //       cy.get('button').contains('Accept').click();
-  //     }
-  //   });
-  // });
+    cy.wait(3000);
 
-  // it('should visit manage-children', () => {
-  //   cy.visit('/manage-children');
-  //   cy.url().should('equal', 'http://localhost:4200/manage-children');
-  // });
+  });
 
-  // it('should select a child and redirect to dashboard', () => {
-  //   cy.get('.circle')
-  //     .first()
-  //     .then(($btn) => {
-  //       if ($btn.text() === 'Add Child') {
-  //         cy.log('no existing children');
-  //       } else {
-  //         cy.visit('/manage-children');
-  //         cy.get('.circle').first().click();
+  it('should load manage-children', () => {
+    cy.url().should('equal', 'http://localhost:4200/manage-children');
+  });
 
-  //         cy.get('.overlay').should('be.visible');
-  //         cy.get('.continueChild').click();
-  //         cy.url().should('equal', 'http://localhost:4200/dashboard');
-  //       }
-  //     });
-  // });
+  it('should load children', () => {
+    cy.get('.circle').should('exist');
+  });
 
-  // it('should redirect to add-child', () => {
-  //   cy.visit('/manage-children');
-  //   cy.get('button').last().click();
-  //   cy.url().should('equal', 'http://localhost:4200/add-child');
-  // });
+  it('should redirect to add-child', () => {
+    cy.get('.circle').last().click();
+    cy.url().should('equal', 'http://localhost:4200/add-child');
+  });
 });
